6583aa2ee83bd457044038855a5a41a873af88fa
Guillaume Giraud <guillaume.giraud@gmail.com>
2021-08-04 13:08:30 +0200

    reflexio: replaced std::vector with std::array for holding member descriptors

--- include/app_utils/relfexio.hpp
+++ include/app_utils/relfexio.hpp
@@ -2,11 +2,10 @@
 
 #include <app_utils/cond_check.hpp>
 #include <app_utils/string_utils.hpp>
-#include <array>
-#include <vector>
 #include <string>
-#include <typeinfo>
 #include <memory>
+#include <array>
+#include <typeinfo>
 
 struct member_descriptor_t {
 
@@ -77,37 +76,53 @@ REFLEXIO_MEMBER_VAR_DEFINE(type2, member2, default_val2, description2);
               std::make_unique<member_descriptor_impl_t<var_type, TypeName>>(&TypeName::var_name, #var_name, app_utils::strutils::to_string(default_value), description)), \
                                 true); \
     } \
-  }; \
+  };\
   var_type var_name = (var_name##_descriptor_t :: register_descriptor(*this), default_value)
 
 
-template<typename CRTP>
+template<typename CRTP, size_t NumMemberVariables>
 struct ReflexioStructBase {
+  using TypeName = CRTP;
 #ifdef REFLEXIO_STRUCT_USE_PYBIND_MODULE
   using PybindClassType = pybind11::class_<CRTP>;
 #endif
 
  private:
-  using member_register_t = std::vector<std::unique_ptr<member_descriptor_t const>>;
-  inline static std::vector<std::unique_ptr<member_descriptor_t const>> member_register;
+  using member_register_t = std::array<std::unique_ptr<member_descriptor_t const>, NumMemberVariables>;  
+  inline static member_register_t s_member_register;
 
  protected:
   static void register_member(std::unique_ptr<member_descriptor_t> member_descriptor) { 
-    member_register.emplace_back(std::move(member_descriptor)); 
+    for (auto& slot : s_member_register) {
+      if (slot == nullptr) {
+        slot = std::move(member_descriptor);
+        break;
+      }
+    }
   }
 
  public:
   static member_register_t const& get_member_descriptors() { 
     static CRTP type; // forces population of member register once
-    return member_register; 
+    return s_member_register; 
   }
   
-  static size_t num_members() { return get_member_descriptors().size(); }
+  static constexpr size_t num_members() { return NumMemberVariables; }
 };
 
+constexpr size_t count_chars(std::string const text, char char_to_count) {
+  size_t count = 0;
+
+  for (size_t i = 0; i < text.size(); i++) {
+    if (text[i] == char_to_count) {
+      count++;
+    }
+  }
+  return count;
+}
+
 #define REFLEXIO_STRUCT_DEFINE(StructName, ...)  \
-  struct StructName : ReflexioStructBase<StructName> {\
-    using TypeName = StructName; \
+  struct StructName : ReflexioStructBase<StructName, count_chars(#__VA_ARGS__, ';')> {\
     __VA_ARGS__;                                 \
   }
 
