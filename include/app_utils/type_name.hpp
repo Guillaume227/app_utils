f5c37301405c4921135ad4c085ffc3a064f9cb97
Guillaume Giraud <guillaume.giraud@gmail.com>
2022-08-05 17:44:02 +0200

    [type_name] added template specializations for (u)int(8|16|32)_t

--- include/app_utils/type_name.hpp
+++ include/app_utils/type_name.hpp
@@ -15,4 +15,56 @@ template<typename T>
 std::string_view typeName(T const& t, bool minimal = false) {
   return parseTypeName(typeid(t).name(), minimal);
 }
+
+template<>
+constexpr std::string_view typeName<uint8_t>(bool) {
+  return "uint8_t";
+}
+template<>
+constexpr std::string_view typeName<int8_t>(bool) {
+  return "int8_t";
+}
+template<>
+constexpr std::string_view typeName<uint16_t>(bool) {
+  return "uint16_t";
+}
+template<>
+constexpr std::string_view typeName<int16_t>(bool) {
+  return "int16_t";
+}
+template<>
+constexpr std::string_view typeName<uint32_t>(bool) {
+  return "uint32_t";
+}
+template<>
+constexpr std::string_view typeName<int32_t>(bool) {
+  return "int32_t";
+}
+
+
+template<>
+constexpr std::string_view typeName(uint8_t const&, bool) {
+  return typeName<uint8_t>();
+}
+template<>
+constexpr std::string_view typeName(int8_t const&, bool) {
+  return typeName<int8_t>();
+}
+template<>
+constexpr std::string_view typeName(uint16_t const&, bool) {
+  return typeName<uint16_t>();
+}
+template<>
+constexpr std::string_view typeName(int16_t const&, bool) {
+  return typeName<int16_t>();
+}
+template<>
+constexpr std::string_view typeName(uint32_t const&, bool) {
+  return typeName<uint32_t>();
+}
+template<>
+constexpr std::string_view typeName(int32_t const&, bool) {
+  return typeName<int32_t>();
+}
+
 }// namespace app_utils
